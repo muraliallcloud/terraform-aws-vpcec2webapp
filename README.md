2bc432afa620954522ec41821616db8247dbe3e4
Murali Uppalapati <muraliallcloud@gmail.com>
2022-06-02 15:24:14 +0530

    Updated Variables Default & Readme file

--- README.md
+++ README.md
@@ -1,12 +1,59 @@
-AWS VPC SG EC2 Creation Terraform module
+**AWS - VPC / IGW / RT / SN / SG / EC2 Creations using this Terraform module**
 
-Terraform module which creates VPC SG EC2 resources on AWS.
+This Terraform module can be used to create below resources:<br/>
+> VPC<br/>
+> Internet Gateway<br/>
+> Route Tables<br/>
+> Subnets<br/>
+> Security Groups<br/>
+> EC2 Instances<br/>
 
-Usage:
+<br/>
 
+**Usage Example:**
+
+```rb
 module "vpcec2webapp" {
-  source = "terraform-aws-modules/vpcec2webapp/aws"
+  source  = "muraliallcloud/vpcec2webapp/aws"
+  version = "0.0.2"
+
+  # Generic configuration
+  region = "ap-south-1"
+  profile = "default"
   application_name = "brickspay"
-  
-  }
+  owners = "Infra"
+  azs = ["ap-south-1a", "ap-south-1b", "ap-south-1c"]
+
+  # Resource Creation Configuration
+  create_vpc_igw_rt_sn = true
+  create_public_sg = true
+  create_private_sg = true
+  create_ec2_linux = true
+  create_ec2_windows = false
+
+  # VPC Configuration
+  vpc_cidr_block = "172.25.0.0/16"
+  all_cidr_block = ["0.0.0.0/0"]
+  igw_cidr_block = "0.0.0.0/0"
+  sn_1a = "172.25.1.0/28"
+  sn_2a = "172.25.2.0/28"
+  sn_1b = "172.25.3.0/28"
+  sn_2b = "172.25.4.0/28"
+
+  # Security Group Configuration
+  ingress_public_sg_ports = ["22", "443", "80", "3389"]
+  ingress_private_sg_ports = ["22","80"]
+
+  # EC2 Configuration
+  linux_ami     = "ami-079b5e5b3971bd10d"
+  windows_ami   = "ami-09ed03e97033b6d21"
+  instance_type = "t2.micro"
+  publicip      = true
+  keyname       = "bookz-privatekey"
+  linuxwebsetupscript_src = "linuxwebsetup.sh"
+  linuxwebsetupscript_dest = "/tmp/linuxwebsetup.sh"
+  ec2_instance_platform = "linux"
+  #ec2_instances_count = 1
+
 }
+```
--- variables.tf
+++ variables.tf
@@ -5,34 +5,31 @@
 variable "region" {
   description = "Region in which AWS resources to be created"
   type = string
-  default = "ap-south-1"
+  default = null
 }
-
-#variable "access_key" {}
-#variable "secret_key" {}
 
 variable "profile" {
   description = "AWS Profile to be used to create resources"
   type = string
-  default = "default"
+  default = null
 }
 
 variable "application_name" {
   description = "Application Name used for prefix of resource names"
   type = string
-  default = "screenz"
+  default = null
 }
 
 variable "owners" {
   description = "Owners Name used for prefix of resource names"
   type = string
-  default = "Infra"
+  default = null
 }
 
 variable "azs" {
   description = "AZ Groups List"
   type    = list(any)
-  default = ["ap-south-1a", "ap-south-1b", "ap-south-1c"]
+  default = null
 }
 
 ####################################
@@ -42,19 +39,19 @@ variable "azs" {
 variable "create_vpc_igw_rt_sn" {
   description = ""
   type = bool
-  default = true
+  default = null
 }
 
 variable "create_public_sg" {
   description = ""
   type = bool
-  default = true
+  default = null
 }
 
 variable "create_private_sg" {
   description = ""
   type = bool
-  default = true
+  default = null
 }
 
 ####################################
@@ -64,43 +61,43 @@ variable "create_private_sg" {
 variable "vpc_cidr_block" {
   description = "CIDR Block to be assigned for VPC"
   type    = string
-  default = "172.25.0.0/16"
+  default = null
 }
 
 variable "all_cidr_block" {
   description = "CIDR Block with all IPs"
   type    = list(any)
-  default = ["0.0.0.0/0"]
+  default = null
 }
 
 variable "igw_cidr_block" {
   description = "IGW CIDR Block"
   type    = string
-  default = "0.0.0.0/0"
+  default = null
 }
 
 variable "sn_1a" {
   description = "CIDR Block to be assigned for Subnet"
   type    = string
-  default = "172.25.1.0/28"
+  default = null
 }
 
 variable "sn_2a" {
   description = "CIDR Block to be assigned for Subnet"
   type    = string
-  default = "172.25.2.0/28"
+  default = null
 }
 
 variable "sn_1b" {
   description = "CIDR Block to be assigned for Subnet"
   type    = string
-  default = "172.25.3.0/28"
+  default = null
 }
 
 variable "sn_2b" {
   description = "CIDR Block to be assigned for Subnet"
   type    = string
-  default = "172.25.4.0/28"
+  default = null
 }
 
 
@@ -111,13 +108,13 @@ variable "sn_2b" {
 variable "ingress_public_sg_ports" {
   description = "Public Security Group Ports"
   type    = list(any)
-  default = ["22", "443", "80", "3389"]
+  default = null
 }
 
 variable "ingress_private_sg_ports" {
   description = "Private Security Group Ports"
   type    = list(any)
-  default = ["22","80"]
+  default = null
 }
 
 
@@ -128,61 +125,61 @@ variable "ingress_private_sg_ports" {
 variable "linux_ami" {
   type = string
   description = ""
-  default     = "ami-079b5e5b3971bd10d"
+  default     = null
 }
 
 variable "windows_ami" {
   type = string
   description = ""
-  default   = "ami-09ed03e97033b6d21"
+  default   = null
 }
 
 variable "instance_type" {
   type = string
   description = ""
-  default = "t2.micro"
+  default = null
 }
 
 variable "publicip" {
   type = bool
   description = ""
-  default      = true
+  default      = null
 }
 
 variable "keyname" {
   type = string
   description = ""
-  default       = "bookz-privatekey"
+  default       = null
 }
 
 variable "linuxwebsetupscript_src" {
   type = string
   description = ""
-  default = "linuxwebsetup.sh"
+  default = null
 }
 
 variable "linuxwebsetupscript_dest" {
   type = string
   description = ""
-  default = "/tmp/linuxwebsetup.sh"
+  default = null
 }
 
 variable "create_ec2_linux" {
   type = bool
   description = ""
-  default = true
+  default = null
 }
 
 variable "create_ec2_windows" {
   type = bool
   description = ""
-  default = false
+  default = null
 }
 
 variable "ec2_instances_count" {
   type = number
   description = "Number of EC2 Instances to be created. Hint- Should be less than 3."
-  default = 1
+  default = null
   validation {
     condition = var.ec2_instances_count <3
     error_message = "The Number of Instances to be created should be less than 3."
@@ -192,11 +189,7 @@ variable "ec2_instances_count" {
 variable "ec2_instance_platform" {
   type = string
   description = "Enter the EC2 Instance platform. Hint- linux / windows ."
-  default = "linux"
+  default = null
 }
 
-
-####################################
-# EC2 Variables
-####################################
 
